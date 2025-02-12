9ec548376707a963bb977add3cd6c10c3c3c774d
Naomi <gabriella.naomi21@ui.ac.id>
2025-02-12 14:02:31 +0700

    try use exec

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
+
 # Gunakan Node.js versi terbaru sebagai base image
+
 FROM node:18
+
 
 # Set direktori kerja di dalam container
 WORKDIR /app
@@ -7,11 +10,6 @@ WORKDIR /app
 # Copy file package.json dan package-lock.json
 COPY package*.json ./
 
-# Install Velero CLI (adjust version if needed)
-RUN curl -LO https://github.com/vmware-tanzu/velero/releases/download/v1.15.2/velero-linux-amd64.tar.gz && \
-    tar -xzf velero-linux-amd64.tar.gz && \
-    mv velero /usr/local/bin && \
-    rm velero-linux-amd64.tar.gz 
 
 # Install dependencies
 RUN npm install
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
+import { exec } from 'node:child_process';
+
 import bodyParser from 'body-parser';
-import * as k8s from '@kubernetes/client-node';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,47 +15,18 @@ app.use(bodyParser.json());
 app.post("/webhook", async (req, res) => {
   if (req.body.status === "firing") {
     console.log("Alert firing! Triggering Velero restore...");
-
-    try {
-      const backupName = "backup-nginx";
-      const restoreName = `restore-${Date.now()}`;
-      const namespace = "default";
-
-      const restore = {
-        apiVersion: 'velero.io/v1',
-        kind: 'Restore',
-        metadata: {
-          name: restoreName,
-          namespace: namespace,
-        },
-        spec: {
-          backupName: backupName,
-          // ... other restore options
-        },
-      };
-
-     
-      const kc = new k8s.KubeConfig();
-      kc.loadFromDefault();
-      console.log("kc "+ kc)
-      
-
-      const customObjectsApi = kc.makeApiClient(k8s.CustomObjectsApi);
-      console.log(restore)
-      console.log(namespace)
-      console.log(customObjectsApi)
-      const response = await customObjectsApi.createNamespacedCustomObject('velero.io', 'v1','default', 'restores', restore, undefined, undefined, undefined, undefined);
-
-      console.log("Velero restore triggered:", response.body);
-      res.status(202).json({ message: "Velero restore triggered", restoreName: restoreName });
-
-    } catch (error) {
-      console.error("Error triggering Velero restore:", error);
-      res.status(500).json({ error: error.message });
-    }
-
-  } else {
-    res.status(200).send("No action needed");
+    const backupName = "backup-nginx"
+    const command = `velero create restore --from-backup ${backupName} --namespace nginx`; // Customize flags as needed
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error creating restore: ${error}`);
+        // Handle error
+      } else {
+        console.log(`Restore created successfully: ${stdout}`);
+        // Handle success
+      }
+    });
+ 
   }
 });
 
